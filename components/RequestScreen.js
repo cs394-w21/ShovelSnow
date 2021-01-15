3080e4bc22d002dcbbfffb27c479f253f62fa19a
Zhihan1996 <zzh760998379@163.com>
2021-01-14 20:19:54 -0600

    add the cancle button

--- components/RequestScreen.js
+++ components/RequestScreen.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
+import React, {useState} from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 function RequestHelpBtn() {
+  const [requested, setRequested] = useState(0);
+  const textInside = ['SHOVEL!', 'CANCEL!'];
+
+  function handleOnPress() {
+    if (requested == 0) {
+      setRequested(1);
+    } else {
+      setRequested(0);
+    }
+  } 
+
   return (
-    <TouchableOpacity onPress={ () => { console.log('PRESSED!'); }}>
-      <Text>SHOVEL!</Text>
+    <TouchableOpacity onPress={handleOnPress}>
+      <Text>{textInside[requested]}</Text>
     </TouchableOpacity>
   );
 }
+
+
 
 
 export default RequestHelpBtn;
